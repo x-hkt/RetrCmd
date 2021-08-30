53580f5251e47f4f8a67c8d8fc99552d26030d7d
x-hkt <89650447+x-hkt@users.noreply.github.com>
2021-08-30 12:33:51 -0400

    finish writing `EIntentConflictException`

--- src/EIntentConflictException.java
+++ src/EIntentConflictException.java
@@ -1,5 +1,18 @@
 public class EIntentConflictException extends Exception {
-	public EIntentConflictException(String classname1, String classname2) {
+	public EIntentConflictException(
+		String classname1, String classname2,
+		Map<String, Object> res1, Map<String, Object> res2
+	) {
+		String res1str = res1
+			.keySet()
+			.stream()
+			.map(mkey -> mkey + "= " + map.get(mkey).toString())
+			.collect(Collectors.joining(", ", "(1) Map { ", " }"));
+		String res2str = res2
+			.keySet()
+			.stream()
+			.map(mkey -> mkey + "= " + map.get(mkey).toString())
+			.collect(Collectors.joining(", ", "(2) Map { ", " }"));
 		super(String.format(
 			"""
 			CONFLICT!!! =========================================== ===CONFLICT===
@@ -7,13 +20,17 @@ public class EIntentConflictException extends Exception {
 			You told me to do multiple different things to the same `ISub`.
 			The first `ISub` instance was of the class [ %s ]
 			The other `ISub` instance was of the class [ %s ]
-			[ FIRST.hasSameTarget(OTHER) ] returned [ true ]
-			-- FIX? maybe try and fix your implementation of `hasSameTarget`
+			[ FIRST.identifyTarget() ] returned [ %s ]
+			[ OTHER.identifyTarget() ] returned [ %s ]
+			-- FIX? maybe try and fix your implementation of `identifyTarget`
+			Remember - map entries will be compared using `equals`, as in
+			... [ ((Object) x).equals((Object) y) ]
+			-- FIX? maybe try and fix your implementation of `equals`
 			Both of these were mentioned in an `EIntent`, and I have no idea which
 			`IntentComponent` I should be invoking!
 			Remember - The same `ISub` cannot be mentioned twice in an `EIntent`
 			-- FIX? remove the duplicate (you have to pick one)
 			"""
-		), classname1, classname2);
+		), classname1, classname2, res1str, res2str);
 	}
 }
