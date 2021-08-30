c98bb39484525de4422632b1525bf3552d7ca098
x-hkt <89650447+x-hkt@users.noreply.github.com>
2021-08-30 16:36:46 -0400

    Create UnaryRetr.java

---
+++ src/UnaryRetr.java
@@ -0,0 +1,12 @@
+public class UnaryRetr<A> implements Retr<A, A> {
+	private final UnaryOperator<Optional<A>> unaryoperator;
+
+	public UnaryRetr(UnaryOperator<Optional<A>> unaryoperator) {
+		this.unaryoperator = unaryoperator;
+	}
+	
+	public Pair<Optional<A>, Optional<A>> get(Optional<A> state) {
+		Optional<A> result = this.unaryoperator.apply(state);
+		return new Pair<>(result, result);
+	}
+}
